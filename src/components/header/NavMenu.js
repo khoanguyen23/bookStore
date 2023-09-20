688c2d2afa736df86c3d9a256ca67d4b7f4c743a
khoanguyen23 <khoahoang151@gmail.com>
2023-09-20 23:28:58 +0700

    init project

---
+++ src/components/header/NavMenu.js
@@ -0,0 +1,222 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+import { multilanguage } from "redux-multilanguage";
+
+const NavMenu = ({ strings, menuWhiteClass, sidebarMenu }) => {
+  return (
+    <div
+      className={` ${
+        sidebarMenu
+          ? "sidebar-menu"
+          : `main-menu ${menuWhiteClass ? menuWhiteClass : ""}`
+      } `}
+    >
+      <nav>
+        <ul>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/"}>
+              {strings["home"]}
+              {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )}
+            </Link>
+            <ul className="mega-menu mega-menu-padding">
+              <li>
+                <ul>   
+                  <li className="mega-menu-title">
+                    <Link to={process.env.PUBLIC_URL + "/"}>
+                      {strings["home_book_store"]}
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="mega-menu-title">
+                    <Link to={process.env.PUBLIC_URL + "/"}>
+                      {strings["home_group_three"]}
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+              {" "}
+              {strings["shop"]}
+              {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )}
+            </Link>
+            <ul className="mega-menu">
+              <li>
+                <ul>
+                  <li className="mega-menu-title">
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+                      {strings["shop_layout"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+                      {strings["shop_grid_standard"]}
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="mega-menu-title">
+                    <Link to={process.env.PUBLIC_URL + "/product/1"}>
+                      {strings["product_details"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product-tab-left/1"}>
+                      {strings["product_tab_left"]}
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="mega-menu-img">
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+                      <img
+                        src={
+                          process.env.PUBLIC_URL +
+                          "/assets/img/banner/banner-12.png"
+                        }
+                        alt=""
+                      />
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+              {strings["collection"]}
+            </Link>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/"}>
+              {strings["pages"]}
+              {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )}
+            </Link>
+            <ul className="submenu">
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/cart"}>
+                  {strings["cart"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/checkout"}>
+                  {strings["checkout"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/wishlist"}>
+                  {strings["wishlist"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/compare"}>
+                  {strings["compare"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/my-account"}>
+                  {strings["my_account"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/login-register"}>
+                  {strings["login_register"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/about"}>
+                  {strings["about_us"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/contact"}>
+                  {strings["contact_us"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/not-found"}>
+                  {strings["404_page"]}
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/blog-standard"}>
+              {strings["blog"]}
+              {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )}
+            </Link>
+            <ul className="submenu">
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/blog-standard"}>
+                  {strings["blog_standard"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/blog-no-sidebar"}>
+                  {strings["blog_no_sidebar"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/blog-right-sidebar"}>
+                  {strings["blog_right_sidebar"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/blog-details-standard"}>
+                  {strings["blog_details_standard"]}
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/contact"}>
+              {strings["contact_us"]}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+NavMenu.propTypes = {
+  menuWhiteClass: PropTypes.string,
+  sidebarMenu: PropTypes.bool,
+  strings: PropTypes.object
+};
+
+export default multilanguage(NavMenu);
